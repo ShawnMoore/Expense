21b014bde310a266816e29b0335a4efa58ac8036
stmf35 <stmf35@mail.missouri.edu>
2015-04-11 17:50:57 -0500

    Created data model classes
    Created classes to store One time expenses and Trip expenses

---
+++ eXpense/eXpense/Model/OneTimeExpense.swift
@@ -0,0 +1,86 @@
+//
+//  OneTimeExpense.swift
+//  eXpense
+//
+//  Created by Shawn Moore on 4/10/15.
+//  Copyright (c) 2015 BoardPaq LLC. All rights reserved.
+//
+
+//  "Id": 1,
+//  "Name": "sample string 2",
+//  "Amount": 3.1,
+//  "Date": "2015-04-03T02:14:46.6757124+00:00",
+//  "Location": "sample string 5",
+//  "Description": "sample string 6",
+//  "PhotoURI": "sample string 7",
+//  "UserId": 8,
+//  "TripId": 1,
+//  "LastSeen": "2015-04-03T02:14:46.6913299+00:00",
+//  "CreatedAt": "2015-04-03T02:14:46.6913299+00:00",
+//  "UpdatedAt": "2015-04-03T02:14:46.6913299+00:00",
+//  "Deleted": true
+
+import UIKit
+
+class OneTimeExpense: NSObject {
+    var name: String
+    var amount: Double
+    var date: String
+    var location: String?
+    var expenseDescription: String?
+    var photoURI: String?
+    var userId: Int?
+    var tripId: Int?
+    var lastSeen: String?
+    var createdAt: String
+    var updatedAt: String?
+    var deleted: Bool
+    
+    init(forName: String, amount: Double, date: String, createdAt: String, deleted: Bool)
+    {
+        self.name = forName
+        self.amount = amount
+        self.date = date
+        self.location = nil
+        self.expenseDescription = nil
+        self.photoURI = nil
+        self.userId = nil
+        self.tripId = nil
+        self.lastSeen = nil
+        self.createdAt = createdAt
+        self.updatedAt = nil
+        self.deleted = deleted
+    }
+    
+    init(forName: String, amount: Double, date: String, createdAt: String, deleted: Bool, location: String, description: String, photoURI: String, userId: Int, tripId: Int, lastSeen: String, updatedAt: String)
+    {
+        self.name = forName
+        self.amount = amount
+        self.date = date
+        self.location = location
+        self.expenseDescription = description
+        self.photoURI = photoURI
+        self.userId = userId
+        self.tripId = tripId
+        self.lastSeen = lastSeen
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.deleted = deleted
+    }
+    
+    init(dict: Dictionary<String, Any>)
+    {
+        self.name = dict["Name"] as String
+        self.amount = dict["Amount"] as Double
+        self.date = dict["Date"] as String
+        self.location = dict["Location"] as? String
+        self.expenseDescription = dict["Description"] as? String
+        self.photoURI = dict["PhotoURI"] as? String
+        self.userId = dict["UserId"] as? Int
+        self.tripId = dict["TripId"] as? Int
+        self.lastSeen = dict["LastSeen"] as? String
+        self.createdAt = dict["CreatedAt"] as String
+        self.updatedAt = dict["UpdatedAt"] as? String
+        self.deleted = dict["Deleted"] as Bool
+    }
+}
---
+++ eXpense/eXpense/Model/TripExpense.swift
@@ -0,0 +1,72 @@
+//
+//  TripExpense.swift
+//  eXpense
+//
+//  Created by Shawn Moore on 4/10/15.
+//  Copyright (c) 2015 BoardPaq LLC. All rights reserved.
+//
+
+import UIKit
+
+class TripExpense: NSObject {
+    var tripId: Int
+    var name: String
+    var startDate: String
+    var endDate: String?
+    var location: String?
+    var expenseDescription: String?
+    var lastSeen: String?
+    var createdAt: String
+    var updatedAt: String?
+    var deleted: Bool
+    var userId: Int
+    var isComplete: Bool
+    
+    init(forName: String, tripId: Int, startDate: String, createdAt: String, deleted: Bool, userId: Int, isComplete: Bool)
+    {
+        self.tripId = tripId
+        self.name = forName
+        self.startDate = startDate
+        self.endDate = nil
+        self.location = nil
+        self.expenseDescription = nil
+        self.lastSeen = nil
+        self.createdAt = createdAt
+        self.updatedAt = nil
+        self.deleted = deleted
+        self.userId = userId
+        self.isComplete = isComplete
+    }
+    
+    init(forName: String, tripId: Int, startDate: String, endDate: String, location: String, description: String, lastSeen: String, createdAt: String, updatedAt: String, deleted: Bool, userId: Int, isComplete: Bool)
+    {
+        self.tripId = tripId
+        self.name = forName
+        self.startDate = startDate
+        self.endDate = endDate
+        self.location = location
+        self.expenseDescription = description
+        self.lastSeen = lastSeen
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.deleted = deleted
+        self.userId = userId
+        self.isComplete = isComplete
+    }
+    
+    init(dict: Dictionary<String, Any>)
+    {
+        self.tripId = dict["Id"] as Int
+        self.name = dict["Name"] as String
+        self.startDate = dict["StartDate"] as String
+        self.endDate = dict["EndDate"] as? String
+        self.location = dict["Location"] as? String
+        self.expenseDescription = dict["Description"] as? String
+        self.lastSeen = dict["LastSeen"] as? String
+        self.createdAt = dict["CreatedAt"] as String
+        self.updatedAt = dict["UpdatedAt"] as? String
+        self.deleted = dict["Deleted"] as Bool
+        self.userId = dict["UserId"] as Int
+        self.isComplete = dict["IsComplete"] as Bool
+    }
+}
