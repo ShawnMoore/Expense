e23c0e1dece812d3128f7ab9a0cce575484ebb39
stmf35 <stmf35@mail.missouri.edu>
2015-05-03 11:12:23 -0500

    Broke update trip into it's own function

--- eXpense/eXpense/Model/Model.swift
+++ eXpense/eXpense/Model/Model.swift
@@ -86,37 +86,7 @@ class Model: NSObject {
         if (self.tripExpenses.count != 0) {
             
             for index in self.tripExpenses.keys.array {
-                
-                if (self.tripExpenses[index] != nil) {
-                    
-                    if(self.tripExpenses[index]!.oneTimeExpenses.count != 0) {
-                        let id = self.tripExpenses[index]!.id
-                        
-                        var self_index = 0
-                        
-                        //If your reading this, I am so so sorry....We dont know why this works....just dont touch it
-                        //For god sakes....DONT TOUCH IT
-                        for ote in self.tripExpenses[index]!.oneTimeExpenses {
-                            if self.tripExpenses[index]!.oneTimeExpenses[self_index].deleted == true {
-                                 self.tripExpenses[index]!.oneTimeExpenses.removeAtIndex(self_index)
-                            } else {
-                                if (self.tripExpenses[index]!.oneTimeExpenses[self_index].tripId != id) {
-                                    let value = self.tripExpenses[index]!.oneTimeExpenses[self_index]
-
-                                    self.tripExpenses[index]!.oneTimeExpenses.removeAtIndex(self_index)
-
-                                    if (value.tripId == nil) {
-                                        self.oneTimeExpenses.append(value)
-                                    } else {
-                                        self.tripExpenses[value.tripId!]?.oneTimeExpenses.append(value)
-                                    }
-                                } else {
-                                    self_index++
-                                }
-                            }
-                        }
-                    }
-                }
+                updateTrip(index)
             }
         }
         
@@ -159,14 +129,6 @@ class Model: NSObject {
                             }
                         }
                     }
-                }
-            }
-        }
-        
-        if (self.tripExpenses.count != 0) {
-            for index in self.tripExpenses.keys.array {
-                if self.tripExpenses[index]!.deleted == true {
-                    self.tripExpenses[index] = nil
                 }
             }
         }
